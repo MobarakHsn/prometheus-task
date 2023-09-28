a53f1c4352ffa7687891b45b5ce2c88642ce56de
MobarakHsn <mobarak@appscode.com>
2023-09-28 19:41:57 +0600

    c1:master Basic setup

---
+++ api/api.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
+
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "No of request handled by Ping Handler",
+	},
+)
+
+func ping(w http.ResponseWriter, req *http.Request) {
+	pingCounter.Inc()
+	fmt.Fprintf(w, "pong")
+}
+
+func StartServer(port string) {
+	port = ":" + port
+	prometheus.MustRegister(pingCounter)
+	http.HandleFunc("/ping", ping)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(port, nil)
+}
